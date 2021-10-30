f19a4d2037c9ecb7cb41401616bba16bd093b486
hardcorequal <hardcorequal@gmail.com>
2021-10-30 12:11:34 +0300

    feat: add cli with oclif

--- webpack_demo/src/index.ts
+++ webpack_demo/src/index.ts
@@ -1,11 +1,4 @@
-import playwright from 'playwright';
-
-export function open() {
-  (async () => {
-    const browser = await playwright.chromium.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await browser.close();
-  })();
+export function hello() {
+  return 'Hello from webpack';
 }
 
